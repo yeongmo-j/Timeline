35407f4d0cf5806c36cbf86faeaa34b9c001514b
yeongmo-j <yeongmoj94@gmail.com>
2019-07-04 00:43:41 +0900

    Prototyping login and registration form in react

--- timeline/src/App.js
+++ timeline/src/App.js
@@ -1,6 +1,10 @@
 import React, {Component} from 'react';
-import history from './History';
+import {history} from './History';
 import { Router, Route, Switch } from "react-router-dom";
+
+import Login from './components/Login';
+import Register from './components/Register';
+import Timeline from './components/Timeline';
 
 import './App.css';
 
@@ -11,7 +15,9 @@ class App extends Component {
       <div>
         <Router history={history}>
           <Switch>
-            <Route path='/temp' exact component={} /> 
+            <Route path='/login' exact component={Login} /> 
+            <Route path='/register' exact component={Register} />
+            <Route path='/timeline' exact component={Timeline} />
           </Switch>
         </Router>
       </div>
